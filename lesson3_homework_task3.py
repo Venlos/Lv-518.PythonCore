5c65b3f1c1f20f18bc93e77de206c0e12cc679b4
Igor Flyunt <igorflyunt2@gmail.com>
2020-07-04 12:53:47 +0300

    lesson3_homework_task3

---
+++ lesson3_homework_task3.py
@@ -0,0 +1,8 @@
+a = int(input('Введіть перше число'))
+b = int(input('Введіть друге число'))
+print('a =', a)
+print('b =', b)
+print('Зміна значень:')
+a, b = b, a
+print('a =', a)
+print('b =', b)
