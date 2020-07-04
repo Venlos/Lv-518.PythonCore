55d3d19c9e56aca119516e0f84048870d33e8311
Igor Flyunt <igorflyunt2@gmail.com>
2020-07-04 14:48:11 +0300

    delete homework 2 and 3

--- lesson3_homework_task3.py
+++
@@ -1,8 +0,0 @@
-a = int(input('Введіть перше число'))
-b = int(input('Введіть друге число'))
-print('a =', a)
-print('b =', b)
-print('Зміна значень:')
-a, b = b, a
-print('a =', a)
-print('b =', b)
