bae652a1e76dffdf538b95d15ab33ae1d0256562
chornyyelena <67710962+chornyyelena@users.noreply.github.com>
2020-07-13 23:30:18 +0300

    Add files via upload

---
+++ Convert boolean.py
@@ -0,0 +1,9 @@
+def bool_to_word(boolean):
+    # T[ODO
+    if boolean == True:
+        return "Yes"
+    else:
+        return 'No'
+
+# return "Yes" if boolean else "No"
+# bool_to_word = {True: 'Yes', False: 'No'}.get
